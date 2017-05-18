d2b483f51f986d38c088d14ee47be7bd24924d83
stpoa <stpoa@icloud.com>
2017-05-18 13:35:52 +0200

    Add testing framework, create first test

---
+++ tests/container.js
@@ -0,0 +1,21 @@
+var tape = require('tape')
+var addAssertions = require('extend-tape')
+var renderer = require('react-test-renderer/shallow').createRenderer()
+var jsxEquals = require('tape-jsx-equals')
+var React = require('react')
+var HighchartsReact = require('../')
+var test = addAssertions(tape, { jsxEquals })
+
+test('renders container', function (t) {
+  t.plan(1)
+  renderer.render(React.createElement(HighchartsReact, {
+    constructor: 'chart',
+    options: { series: [{ data: [1, 2, 3] }] }
+  }))
+  var actual = renderer.getRenderOutput()
+
+  t.jsxEquals(
+    actual,
+    React.createElement('div', { ref: function noRefCheck () {} })
+  )
+})
