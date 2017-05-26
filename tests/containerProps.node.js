d76950fd5b5c038441e111d11a29bd3b0a0d5d2f
stpoa <stpoa@icloud.com>
2017-05-26 13:07:34 +0200

    Add browser testing environment, add test

---
+++ tests/containerProps.node.js
@@ -0,0 +1,15 @@
+import tape from 'tape'
+import React from 'react'
+import HighchartsReact from '../'
+import createComponent from 'react-unit'
+
+const test = tape
+
+test('passess container props', (t) => {
+  t.plan(1)
+  var component = createComponent.shallow(
+    <HighchartsReact containerProps={{ className: 'my-custom-class-name' }} />
+  )
+
+  t.equal(component.props.className, 'my-custom-class-name')
+})
