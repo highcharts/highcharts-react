7e6637c7886d2e1779e7b0ce89634fa8f4fdd515
ppotaczek <ppotaczek8@gmail.com>
2020-02-12 16:51:58 +0100

    Added new test structure.

---
+++ tests/__tests__/component.test.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { shallow, mount, render } from 'enzyme';
---
+++ tests/__tests__/props.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
